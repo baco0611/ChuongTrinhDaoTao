ce5948a5b2b67157262a68ee061150e1125fb954
vananh2003a2 <ntvanh2003a2@gmail.com>
2024-06-16 22:36:48 +0700

    [CREATE] Creat project

---
+++ backend/ChuongTrinhDaoTao/src/main/java/java/com/springboot/ChuongTrinhDaoTaoApplication.java
@@ -0,0 +1,13 @@
+package java.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuongTrinhDaoTaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuongTrinhDaoTaoApplication.class, args);
+	}
+
+}
---
+++ backend/ChuongTrinhDaoTao/src/test/java/java/com/springboot/ChuongTrinhDaoTaoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuongTrinhDaoTaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
