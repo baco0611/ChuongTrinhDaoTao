932b4188b874d320b4c6ef03bb87d1fc9bf7678c
baco0611 <baoblink@gmail.com>
2024-08-25 17:13:48 +0900

    [CREATE] Create dropdrag function and autosave when drag

--- review/src/views/EditorSection/SectionD/PLOBlock.jsx
+++ review/src/views/EditorSection/SectionD/PLOBlock.jsx
@@ -4,11 +4,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareMinus, faSquarePlus } from '@fortawesome/free-solid-svg-icons'
 import { changeDataSectionD, handleCreatePLO, handleDeletePLO, handleSaveChangeElement } from '../database/sectionD'
 import { UserContext } from '../../../context/ContextProvider'
+import { Draggable, Droppable } from 'react-beautiful-dnd'
 
 export default function PLOBlock({ data, setState }) {
     const { setIsDataSaved, serverAPI, apiURL, token } = useContext(UserContext)
     const [ isDisable, setIsDisable ] = useState(false)
     const { id } = useParams()
+    // console.log(data)
 
     useEffect(() => {
         const elements = document.querySelectorAll('textarea[name="content"]')
@@ -20,114 +22,141 @@ export default function PLOBlock({ data, setState }) {
     })
 
     return (
-        <div className='PLO-block'>
-            <h2>{data.typeIndex}. {data.title} </h2>
-            <div
-                className='table'
-            >
-                <div className='head'>
-                    <div className='row'>
-                        <div className='block'><p>Ký hiệu</p></div>
-                        <div className='block'><p>Chủ đề chuẩn đầu ra</p></div>
-                        <div className='block'><p>Thang đánh giá năng lực</p></div>
-                        <div className='block'>
-                        <button
-                                disabled={isDisable}
-                                onClick={() => handleCreatePLO({
-                                    api: serverAPI,
-                                    token,
-                                    numOfElement: data.data.length,
-                                    type: data.type,
-                                    typeDetail: data.typeDetail,
-                                    typeIndex: data.typeIndex,
-                                    programId: id,
-                                    setState,
-                                    setIsDisable,
-                                    setIsDataSaved,
-                                })}
-                            >
-                                <FontAwesomeIcon icon={faSquarePlus} />
-                            </button>
-                        </div>
-                    </div>
-                </div>    
-                <div className='body'>
-                {
-                    data.data.map((element, index) => {
-                        {/* console.log(element) */}
-                        return (
-                            <div 
-                                className='row'
-                                key={index}
-                            >
-                                <div className='block'><label htmlFor={`${data.type}-${index}`}>{element.symbol}</label></div>
+        <Droppable
+            droppableId={`${data.type}/${data.typeDetail}`}
+            type="PLO"
+        >
+        {
+            provider => (
+                <div 
+                    className='PLO-block'
+                    ref={provider.innerRef}
+                    {...provider.droppableProps}
+                >
+                    <h2>{data.typeIndex}. {data.title} </h2>
+                    <div
+                        className='table'
+                    >
+                        <div className='head'>
+                            <div className='row'>
+                                <div className='block'><p>Ký hiệu</p></div>
+                                <div className='block'><p>Chủ đề chuẩn đầu ra</p></div>
+                                <div className='block'><p>Thang đánh giá năng lực</p></div>
                                 <div className='block'>
-                                    <textarea
-                                        id={`${data.type}-${index}`}
-                                        name='content'
-                                        value={element.content || ""}
-                                        onChange={(e) => changeDataSectionD({ 
-                                            e, 
-                                            id: element.id,
-                                            type: element.type, 
-                                            typeDetail: element.typeDetail,
-                                            setState, 
-                                            setIsDataSaved,
-                                        })}
-                                        onBlur={() => handleSaveChangeElement({
-                                            api: serverAPI,
-                                            id: element.id,
-                                            token,
-                                            content: element.content,
-                                            competency: element.competency,
-                                            setIsDataSaved
-                                        })}
-                                    />
-                                </div>
-                                <div className='block'>
-                                    <textarea
-                                        value={element.competency || ""}
-                                        name='competency'
-                                        style={{height: "100%"}}
-                                        onChange={(e) => changeDataSectionD({ 
-                                            e, 
-                                            id: element.id,
-                                            type: element.type, 
-                                            typeDetail: element.typeDetail,
-                                            setState, 
-                                            setIsDataSaved,
-                                        })}
-                                        onBlur={() => handleSaveChangeElement({
-                                            api: serverAPI,
-                                            id: element.id,
-                                            token,
-                                            content: element.content,
-                                            competency: element.competency,
-                                            setIsDataSaved
-                                        })}
-                                    />
-                                </div>
-                                <div className='block'> 
-                                    <button
-                                        onClick={() => handleDeletePLO({
+                                <button
+                                        disabled={isDisable}
+                                        onClick={() => handleCreatePLO({
                                             api: serverAPI,
                                             token,
-                                            id: element.id,
-                                            setState, 
-                                            symbol: element.symbol,
-                                            type: element.type,
+                                            numOfElement: data.data.length,
+                                            type: data.type,
+                                            typeDetail: data.typeDetail,
+                                            typeIndex: data.typeIndex,
+                                            programId: id,
+                                            setState,
+                                            setIsDisable,
                                             setIsDataSaved,
                                         })}
                                     >
-                                        <FontAwesomeIcon icon={faSquareMinus} />
+                                        <FontAwesomeIcon icon={faSquarePlus} />
                                     </button>
                                 </div>
                             </div>
-                        )
-                    })
-                }
+                        </div>    
+                        <div className='body'>
+                        {
+                            data.data.map((element, index) => {
+                                {/* console.log(element) */}
+                                return (
+                                    <Draggable
+                                        draggableId={`drag-${data.typeDetail}-${index}`}
+                                        index={index}
+                                        key={index}
+                                    >
+                                    {
+                                        provided => (
+                                            <div 
+                                                className='row'
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                ref={provided.innerRef}
+                                            >
+                                                <div className='block'><label htmlFor={`${data.type}-${index}`}>{element.symbol}</label></div>
+                                                <div className='block'>
+                                                    <textarea
+                                                        id={`${data.type}-${index}`}
+                                                        name='content'
+                                                        value={element.content || ""}
+                                                        onChange={(e) => changeDataSectionD({ 
+                                                            e, 
+                                                            id: element.id,
+                                                            type: element.type, 
+                                                            typeDetail: element.typeDetail,
+                                                            setState, 
+                                                            setIsDataSaved,
+                                                        })}
+                                                        onBlur={() => handleSaveChangeElement({
+                                                            api: serverAPI,
+                                                            id: element.id,
+                                                            token,
+                                                            content: element.content,
+                                                            competency: element.competency,
+                                                            setIsDataSaved
+                                                        })}
+                                                    />
+                                                </div>
+                                                <div className='block'>
+                                                    <textarea
+                                                        value={element.competency || ""}
+                                                        name='competency'
+                                                        style={{height: "100%"}}
+                                                        onChange={(e) => changeDataSectionD({ 
+                                                            e, 
+                                                            id: element.id,
+                                                            type: element.type, 
+                                                            typeDetail: element.typeDetail,
+                                                            setState, 
+                                                            setIsDataSaved,
+                                                        })}
+                                                        onBlur={() => handleSaveChangeElement({
+                                                            api: serverAPI,
+                                                            id: element.id,
+                                                            token,
+                                                            content: element.content,
+                                                            competency: element.competency,
+                                                            setIsDataSaved
+                                                        })}
+                                                    />
+                                                </div>
+                                                <div className='block'> 
+                                                    <button
+                                                        onClick={() => handleDeletePLO({
+                                                            api: serverAPI,
+                                                            token,
+                                                            id: element.id,
+                                                            setState, 
+                                                            symbol: element.symbol,
+                                                            type: element.type,
+                                                            setIsDataSaved,
+                                                        })}
+                                                    >
+                                                        <FontAwesomeIcon icon={faSquareMinus} />
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+                                    </Draggable>
+                                )
+                            })
+                        }
+                        </div>
+                    </div>
+                    {provider.placeholder}
                 </div>
-            </div>
-        </div>
+            )
+        }
+
+        </Droppable>
     )
 }
--- review/src/views/EditorSection/SectionD/SectionD.jsx
+++ review/src/views/EditorSection/SectionD/SectionD.jsx
@@ -8,6 +8,8 @@ import { useQuery } from 'react-query'
 import Loader from '../../../components/Loader/Loader'
 import { getDataSectionD } from '../database/sectionD'
 import SectionDModule from './SectionDModule'
+import { DragDropContext } from "react-beautiful-dnd"
+import { handleDragEnd } from './handleDragDrop'
 
 export default function SectionD() {
 
@@ -153,21 +155,30 @@ export default function SectionD() {
                 </div>
 
                 <div className='content'>
-                    <SectionDModule
-                        title={"1. KIẾN THỨC"}
-                        data={sectionDValue.KIEN_THUC}
-                        setState={setSectionDValue}
-                    />
-                    <SectionDModule
-                        title={"2. KỸ NĂNG"}
-                        data={sectionDValue.KY_NANG}
-                        setState={setSectionDValue}
-                    />
-                    <SectionDModule
-                        title={"3. THÁI ĐỘ"}
-                        data={sectionDValue.THAI_DO}
-                        setState={setSectionDValue}
-                    />
+                    <DragDropContext onDragEnd={e => handleDragEnd({
+                        e,
+                        api: serverAPI,
+                        token,
+                        setState: setSectionDValue,
+                        setIsDataSaved,
+                        data: sectionDValue
+                    })}>
+                        <SectionDModule
+                            title={"1. KIẾN THỨC"}
+                            data={sectionDValue.KIEN_THUC}
+                            setState={setSectionDValue}
+                        />
+                        <SectionDModule
+                            title={"2. KỸ NĂNG"}
+                            data={sectionDValue.KY_NANG}
+                            setState={setSectionDValue}
+                        />
+                        <SectionDModule
+                            title={"3. THÁI ĐỘ"}
+                            data={sectionDValue.THAI_DO}
+                            setState={setSectionDValue}
+                        />
+                    </DragDropContext>
                 </div>
             </div>
             <EditorFooter
