51fe52e35572ef4c0075cdc978871023ed3513ba
baco0611 <baoblink@gmail.com>
2024-10-05 21:20:00 +0900

    Memo responsibility

--- review/src/views/LecturerManage/Responsibility/responsibility-function.js
+++ review/src/views/LecturerManage/Responsibility/responsibility-function.js
@@ -1,14 +1,13 @@
 import Swal from 'sweetalert2'
 import { postData } from '../../../utils/function';
 
-
 export const changeResponsibility = async ({ api, token, element, department, type, setState, setIsDataSaved }) => {
     if (type == "1") return;
-    let title, text, payload;
+    let title, html, payload;
 
     if (type == "0") {
         title = "PHÂN CÔNG PHỤ TRÁCH";
-        text = `Bạn có muốn phân công giảng viên ${element.lecturerName} phụ trách đơn vị ${department.name} không?`;
+        html = `Bạn có muốn phân công giảng viên <strong>${element.lecturerName}</strong> phụ trách đơn vị <strong>${department.name}</strong> không?`;
         payload = {
             ...element,
             ...department,
@@ -16,13 +15,13 @@ export const changeResponsibility = async ({ api, token, element, department, ty
         };
     } else if (type == "2") {
         title = "HỦY PHÂN CÔNG PHỤ TRÁCH";
-        text = `Bạn có muốn hủy phân công phụ trách đơn vị ${department.name} không?`;
+        html = `Bạn có muốn hủy phân công phụ trách đơn vị <strong>${department.name}</strong> không?`;
         payload = { ...department };
     }
 
     const result = await Swal.fire({
         title,
-        text,
+        html,  // Sử dụng html thay cho text để dùng thẻ HTML
         icon: "warning",
         showCancelButton: true,
         confirmButtonText: "Có",
